e7e91e4fdb2a8f541dbe400228d132f8a7169405
jxchen26 <41722801+jxchen26@users.noreply.github.com>
2019-11-27 19:05:26 -0800

    migrated project from repl.it

---
+++ Python project for student/Mile stone projects/python dungeon crawler/items.py
@@ -0,0 +1,105 @@
+SHOP= ["Amazon Basic Sword","wooden fence shield","wooden sword","iron pike", "wizard staff","???","iron shield"]
+LOOT = ["hp orb", "Pan Lid","Amazon Basic Staff"]
+RUIN = []
+
+DAY_10 = ["Phantom Blade","Sword of the Forgotten","Super Nova Staff"]
+
+
+
+
+ITEM_DATABASE = {
+
+"Amazon Basic Sword":{
+  "name":"Amazon Basic Sword",
+  "atk":10,
+  "def":5,
+  "hp":5,
+  "magic":5,
+  "cost":120
+},
+
+"Amazon Basic Staff":{
+  "name":"Amazon Basic Staff",
+  "atk": 5,
+  "def": 5,
+  "hp" : 5,
+  "magic": 10,
+  "cost":120
+},
+
+"Pan Lid":{
+  "name":"Pan Lid",
+  "def":10,
+  "hp":10,
+},
+
+"wooden fence shield":{
+  "name":"wooden fence shield",
+  "def":10,
+  "cost":50
+},
+
+"wooden sword":{
+  "name":"wooden sword",
+  "atk":10,
+  "cost":50
+},
+
+"???":{
+  "name":"???",
+  "atk":1000,
+  "magic":1000,
+  "cost":10000
+},
+
+"iron pike":{
+  "name":"iron pike",
+  "atk":50,
+  "cost":350
+},
+
+"hp orb":{
+  "name":"hp orb",
+  "hp": 100,
+  "cost":350,
+},
+
+"wizard staff":{
+  "name":"wizard staff",
+  "magic":50,
+  "cost":250
+},
+
+"iron shield":{
+  "name":"iron shield",
+  "hp": 50,
+  "def":50,
+  "cost":350
+},
+
+
+"Phantom Blade":{
+  "name":"Phantom Blade",
+  "atk":150,
+  "magic":20,
+  "cost:":1200
+},
+
+"Sword of the Forgotten":{
+  "name":"Sword of the Forgotten",
+  "atk":100,
+  "hp":100,
+  "cost":1500
+},
+
+"Super Nova Staff":{
+  "name":"Super Nova Staff",
+  "atk":10,
+  "magic":200,
+  "cost":1700
+},
+
+
+}
+
+
